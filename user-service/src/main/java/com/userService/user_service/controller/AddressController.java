e40b9b8ffe6ebd2bdb575d76b6d127cf97f931c5
JSARVAD <jayeshsarvade90@gmail.com>
2024-06-20 10:54:29 +0530

    commit all

--- user-service/src/main/java/com/userService/user_service/controller/AddressController.java
+++ user-service/src/main/java/com/userService/user_service/controller/AddressController.java
@@ -2,17 +2,21 @@ package com.userService.user_service.controller;
 
 import com.userService.user_service.dto.AddressDto;
 
+import com.userService.user_service.entity.Address;
 import com.userService.user_service.payload.AddressResponse;
 import com.userService.user_service.payload.ApiResponse;
 import com.userService.user_service.payload.AppConstants;
 import com.userService.user_service.service.AddressService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/address/api")
@@ -21,6 +25,14 @@ public class AddressController {
 	@Autowired
 	private AddressService addressService;
 
+	@Operation(summary = "Get Address By UserId")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Found the address",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Address.class)) }),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Invalid id supplied",
+					content = @Content),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "address not found",
+					content = @Content) })
 	@GetMapping("/userId/{userId}")
 	public ResponseEntity<AddressDto> getAddressByUserId(@PathVariable("userId") int userId) {
 
@@ -28,24 +40,49 @@ public class AddressController {
 		return new ResponseEntity<AddressDto>(addressByUserId, HttpStatus.OK);
 	}
 
+	@Operation(summary = "Get Address By AddressId")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Found the address",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Address.class)) }),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Invalid id supplied",
+					content = @Content),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "address not found",
+					content = @Content) })
 	@GetMapping("/{id}")
 	public ResponseEntity<AddressDto> getAddress(@PathVariable int id) {
 		AddressDto address = addressService.getAddress(id);
 		return new ResponseEntity<>(address, HttpStatus.OK);
 	}
 
+	@Operation(summary = "Update Address By Its Id")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Address Updated Successfully",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Address.class)) }),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Invalid id supplied",
+					content = @Content),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Address not found",
+					content = @Content) })
 	@PutMapping("/{id}")
 	public ResponseEntity<AddressDto> updateAddress(@Valid @RequestBody AddressDto addressDto, @PathVariable int id) {
 		AddressDto updateAddress = addressService.updateAddress(addressDto, id);
 		return new ResponseEntity<AddressDto>(updateAddress, HttpStatus.OK);
 	}
 
+	@Operation(summary = "Create Address", description = "creating address based on userID")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Address Created",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Address.class)) })})
 	@PostMapping("/userId/{userId}")
 	public ResponseEntity<AddressDto> createAddress(@Valid @RequestBody AddressDto addressDto, @PathVariable int userId) {
 		AddressDto address = addressService.createAddress(addressDto, userId);
 		return new ResponseEntity<AddressDto>(address, HttpStatus.CREATED);
 	}
 
+	@Operation(summary = "Get All Address")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Found the address",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Address.class)) }),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Address not found") })
 	@GetMapping("/")
 	public ResponseEntity<AddressResponse> getAllAddress(
 			@RequestParam(value = "pageNo", defaultValue = AppConstants.PAGE_NO, required = false) int pageNo,
@@ -56,6 +93,14 @@ public class AddressController {
 		return new ResponseEntity<AddressResponse>(allAddress, HttpStatus.OK);
 	}
 
+	@Operation(summary = "Delete Address By UserId")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Address Deleted",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Address.class)) }),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Invalid id supplied",
+					content = @Content),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Address not found",
+					content = @Content) })
 	@DeleteMapping("/userId/{userId}")
 	public ResponseEntity<ApiResponse> deleteAddressByUserId(@PathVariable int userId) {
 		addressService.deleteAddressByUserId(userId);
@@ -63,6 +108,14 @@ public class AddressController {
 				HttpStatus.OK);
 	}
 
+	@Operation(summary = "Delete Address By ID")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Address Deleted",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Address.class)) }),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Invalid id supplied",
+					content = @Content),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Address not found",
+					content = @Content) })
 	@DeleteMapping("/{id}")
 	public ResponseEntity<ApiResponse> deleteAddress(@PathVariable int id) {
 		addressService.deleteAddress(id);
