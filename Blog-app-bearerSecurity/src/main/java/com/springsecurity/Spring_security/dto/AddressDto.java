b58bf50d45947431a9a13fd1d8258ad0b98caff4
JSARVAD <jayeshsarvade90@gmail.com>
2024-06-26 14:47:23 +0530

    commit all

--- Blog-app-bearerSecurity/src/main/java/com/springsecurity/Spring_security/dto/AddressDto.java
+++ Blog-app-bearerSecurity/src/main/java/com/springsecurity/Spring_security/dto/AddressDto.java
@@ -1,5 +1,6 @@
 package com.springsecurity.Spring_security.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -9,9 +10,24 @@ import lombok.*;
 @Builder
 public class AddressDto {
     private int id;
+
+    @NotBlank(message = "Lane1 cannot be blank")
+    @Size(max = 255, message = "Lane1 cannot exceed 255 characters")
     private String lane1;
+
+    @Size(max = 255, message = "Lane2 cannot exceed 255 characters")
     private String lane2;
+
+    @NotBlank(message = "City cannot be blank")
+    @Size(max = 100, message = "City cannot exceed 100 characters")
     private String city;
+
+    @NotBlank(message = "State cannot be blank")
+    @Size(max = 100, message = "State cannot exceed 100 characters")
     private String state;
+
+    @NotNull(message = "Zip code cannot be null")
+    @Min(value = 10000, message = "Zip code must be at least 5 digits")
+    @Max(value = 99999, message = "Zip code cannot exceed 5 digits")
     private int zip;
 }
